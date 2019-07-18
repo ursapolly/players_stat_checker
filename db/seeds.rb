dfc01d358256988315e2631d0a1878dfee39f5aa
Polina Janochkina <apollinariya2209@yandex.ru>
2019-07-18 12:23:04 +0300

    Created seeds for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+2.times do
+  Team.create(
+    name: Faker::Sports::Football.team
+  )
+end
+
+10.times do
+  Player.create(
+    name: Faker::Sports::Football.player,
+    team_id: Team.first.id
+  )
+
+  Player.create(
+    name: Faker::Sports::Football.player,
+    team_id: Team.last.id
+  )
+end
+
+5.times do
+  Match.create(
+    title: Faker::Creature::Animal.name
+  )
+end
+
+Match.all.each do |match|
+  TeamMatch.create(
+    team_id: Team.first.id,
+    match_id: match.id
+  )
+
+  TeamMatch.create(
+    team_id: Team.last.id,
+    match_id: match.id
+  )
+end
+
+Player.all.each do |player|
+  PlayerMatch.create(
+    player_id: player.id,
+    match: Match.find(rand(1..5))
+  )
+end
+
+feature_list = ['Scored 5 goals', 'Ran 10 km', 'Passed 50% of balls']
+
+PlayerMatch.all.each do |pm|
+  feature_list.each do |item|
+    PlayerFeature.create(
+      title: item,
+      player_match_id: pm.id
+    )
+  end
+end
